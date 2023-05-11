88ff51789c48eda006fd6f25a0c75dcfaf7cadfc
Liam Arbuckle <liam@skinetics.tech>
2023-05-11 10:22:38 +1000

    🍏🪐 ↝ FastAPI + turbo => not working

--- components/Connection/fastApi.tsx
+++ components/Connection/fastApi.tsx
@@ -1,34 +1,44 @@
-import { useState } from "react";
+import { useState } from 'react'
 
-export default function GptFast(): JSX.Element {
-  const [text, setText] = useState("");
-  const [response, setResponse] = useState("");
+type Response = {
+  text: string
+}
+
+type Input = {
+  text: string
+}
+
+export default function Home() {
+  const [input, setInput] = useState<Input>({ text: "" })
+  const [response, setResponse] = useState<Response>({ text: "" })
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInput({ text: event.target.value })
+  }
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    const response = await fetch("http://127.0.0.1:8000/submit", {
+    event.preventDefault()
+    const response = await fetch("http://127.0.0.1:8000/generate", {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ text }),
-    });
-
-    const data = await response.json();
-    setResponse(data.response);
-  };
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(input)
+    })
+    const data = await response.json()
+    setResponse({ text: data.text })
+  }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Text:
-        <input
-          type="text"
-          value={text}
-          onChange={(event) => setText(event.target.value)}
-        />
-      </label>
-      <button type="submit">Submit</button>
-      {response && <p>{response}</p>}
-    </form>
-  );
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter text:
+          <input type="text" value={input.text} onChange={handleChange} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      <p>{response.text}</p>
+    </div>
+  )
 }
--- python/main.py
+++ python/main.py
@@ -1,27 +1,43 @@
-import openai_secret_manager
-import openai
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import httpx
+
+class Input(BaseModel):
+    text: str
 
 app = FastAPI()
 
-# Load the OpenAI API key
-secrets = openai_secret_manager.get_secret("openai")
-openai.api_key = secrets["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"]
+# Add CORS middleware to allow cross-origin requests
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.post("/submit")
-async def submit(text: str):
-    # Send the input to the OpenAI API and receive a response
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=text,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
-
-    # Extract the response text from the API response
-    response_text = response.choices[0].text.strip()
-
-    # Return the response text as a JSON object
-    return {"response": response_text}
+@app.post("/generate")
+async def generate(request: Request, input: Input):
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            "https://api.openai.com/v1/engines/gpt-3.5-turbo",
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": "Bearer "
+            },
+            json={
+                "prompt": input.text,
+                "max_tokens": 50,
+                "temperature": 0.7,
+                "n": 1,
+                "stop": "\n"
+            }
+        )
+        print(response.status_code)
+        print(response.text)
+        if response.status_code == 200:
+            return JSONResponse(content={"text": response.json()["choices"][0]["text"]})
+        else:
+            return JSONResponse(content={"text": "Error"})
