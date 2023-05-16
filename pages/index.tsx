e18d1f3172ebaeda51a8daa81a7f373af5cf6b37
andrepology <andrevacha@gmail.com>
2023-05-15 17:01:43 -0700

    fix: add resize handle and delay canvas render

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,15 +9,21 @@ import ChatSide from "@/components/Chat/Chat";
 import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels";
 
 const Home: NextPage = () => {
+
+  const [isResizing, setIsResizing] = useState(false);
+
   return (
     <>
       <PanelGroup direction="horizontal">
-        <Panel defaultSize={50} minSize={50}>
+        <Panel defaultSize={50} minSize={10}>
           <ChatSide />
         </Panel>
-        <PanelResizeHandle />
-        <Panel defaultSize={50} minSize={50}>
-          <iframe src='anotate.pdf' width="100%" height="100%" />
+        <PanelResizeHandle
+          className="w-10 h-10 bg-gray-600 hover:bg-gray-700"
+          onDragging={ (isDragging) => setIsResizing(isDragging) }
+        />
+        <Panel defaultSize={50} minSize={10}>
+          {isResizing ? null : <iframe src='anotate.pdf' width="100%" height="100%" />}
         </Panel>
       </PanelGroup>
     </>
