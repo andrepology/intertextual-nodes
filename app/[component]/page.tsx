402db8ac763a0e209b7e4f75efa2fede712885dd
andrepology <andrevacha@gmail.com>
2023-05-15 18:58:06 -0700

    increment: add nested routes for filestream nav

---
+++ app/[component]/page.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next";
+
+
+export const Component: NextPage = () => {
+
+    
+    
+    return (
+        <div className="m-2 border w-full h-full">
+            <iframe src='anotate.pdf' width="100%" height="100%" />
+        </div>
+    )
+}
+
+export default Component;
