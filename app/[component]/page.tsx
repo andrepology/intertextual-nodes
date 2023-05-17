e1ef7c41b02d431005710c00f354ecc524ab85e5
andrepology <andrevacha@gmail.com>
2023-05-16 22:59:31 -0700

    partial: add basic routing between files

--- app/[component]/page.tsx
+++ app/[component]/page.tsx
@@ -1,15 +1,61 @@
+'use client'
+
 import { NextPage } from "next";
+import Link from "next/link";
+
+import { usePathname, useSearchParams } from "next/navigation";
 
 
-export const Component: NextPage = () => {
+// a sample state object tree representing the full research component
 
-    
-    
+const ResearchNode = {
+    nodeName: "Capybara bathing",
+    nodeDescription: "foo bar",
+    nodeTags: ["#foo"],
+    url: "/",
+    nodeComponents: [
+        {
+            componentName: "manuscript",
+            componentDescription: "foo bar",
+            componentTags: ["#foo"],
+            componentData: "anotate.pdf",
+            componentType: "pdf",
+            url: "/manuscript",
+        },
+        {
+            componentName: "Code",
+            componentDescription: "foo bar",
+            componentTags: ["#foo"],
+            url: "/code"
+        },
+    ]
+}
+
+
+export const ResearchComponent: NextPage = () => {
+
+    const urlPath = usePathname()
+    const params = useSearchParams()
+    const viewMode = Boolean(params?.get("view")) || false
+
     return (
         <div className="m-2 border w-full h-full">
-            <iframe src='anotate.pdf' width="100%" height="100%" />
+
+            Component metadata
+
+            <div>
+                <div> Component Name: Capybara bathing </div>
+                <div> Component Description: foo bar </div>
+                <div> Component Tags: #foo </div>
+            </div>
+
+
+
+            <Link href = { `${urlPath}?view=true` }> View Component </Link>
+
+            {viewMode && <iframe src='anotate.pdf' width="100%" height="100%" />}
         </div>
     )
 }
 
-export default Component;
+export default ResearchComponent;
